4df2ecdaae04601dce407c8cc922dd0c82a9e10d
delenjer <delenjer@gmail.com>
2020-11-11 20:30:48 +0200

    add details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import * as selectors from '../store/store';
 import { loadInfo } from '../store/thunk/thunk';
@@ -24,30 +24,55 @@ export const Details = () => {
               artObject !== undefined && (
                 <section className="details">
                   <div className="wrapper">
+                    <Link
+                      to="/"
+                      className="back"
+                    >
+                      <span className="icon-point-left icon" />
+                      Back
+                    </Link>
+
                     <h1 className="details__title">{artObject.title}</h1>
 
                     <ul className="details__list">
                       {
                         artObject.principalMakers.map(detail => (
-                          <>
+                          <div key={detail.dateOfBirth}>
                             <li className="details__item">
+                              <span className="details__item--title">
+                                Author:
+                              </span>
+
                               {detail.name}
                             </li>
 
                             <li className="details__item">
-                              <span>Period of life:</span>
+                              <span className="details__item--title">
+                                Period of life:
+                              </span>
 
                               {detail.dateOfBirth}
                               {' - '}
                               {detail.dateOfDeath}
                             </li>
-                          </>
+                          </div>
                         ))
                       }
 
                       <li className="details__item">
-                        <span>Dating:</span>
+                        <span className="details__item--title">
+                          Dating:
+                        </span>
+
                         {artObject.dating.presentingDate}
+                      </li>
+
+                      <li className="details__item">
+                        <span className="details__item--title">
+                          Way of drawing:
+                        </span>
+
+                        {artObject.physicalMedium}
                       </li>
                     </ul>
 
