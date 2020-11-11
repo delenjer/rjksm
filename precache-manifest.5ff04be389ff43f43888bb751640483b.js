cb2a316ff50afcb13ceb6ac733aaac8826500a60
delenjer <delenjer@gmail.com>
2020-11-11 20:33:42 +0200

    Updates

---
+++ precache-manifest.5ff04be389ff43f43888bb751640483b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b9dd431d4d0d9ba58dfbacbd6c0c2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8af3891341987e839a1f",
+    "url": "./static/css/main.2f9a3964.chunk.css"
+  },
+  {
+    "revision": "ae7faa2ad7c5e739b875",
+    "url": "./static/js/2.375f8153.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.375f8153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af3891341987e839a1f",
+    "url": "./static/js/main.c1a63cf4.chunk.js"
+  },
+  {
+    "revision": "53d1114eedfc0eaca354",
+    "url": "./static/js/runtime-main.a6df9e19.js"
+  },
+  {
+    "revision": "2c3822205d93075f8d0cdef030e27e47",
+    "url": "./static/media/icomoon.2c382220.eot"
+  },
+  {
+    "revision": "74fcf93fce7278af17a5f98bd7630912",
+    "url": "./static/media/icomoon.74fcf93f.ttf"
+  },
+  {
+    "revision": "9de38cee45bf3396bc480b83316cf97d",
+    "url": "./static/media/icomoon.9de38cee.svg"
+  },
+  {
+    "revision": "baf1c4f82e07ed8758144ed80dfb5cd0",
+    "url": "./static/media/icomoon.baf1c4f8.woff"
+  }
+]);
